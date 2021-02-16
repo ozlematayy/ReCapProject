9e69bcb5dede379cbf652e5a7203fceb25734a5e
melek9341@outlook.com <melek9341@outlook.com>
2021-02-16 20:26:53 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleUI
     class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             CarManager carManager = new CarManager(new EfCarDal());
             var result = carManager.GetCarDetailDtos();
             if (result.Success == true)
@@ -24,7 +24,17 @@ namespace ConsoleUI
             {
                 Console.WriteLine( result.Message ); 
             }
-
+            */
+            Car car = new Car()
+            {
+                BrandId = 3,
+                ColorId = 1,
+                DailyPrice = 200,
+                Description = "Manuel Benzin",
+                ModelYear = "2015"
+            };
+            CarManager carManager = new CarManager(new EfCarDal());
+            carManager.Add(car);
 
         }
     }
